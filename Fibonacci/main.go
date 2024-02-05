c0b249dfbad02fc55721766df5b72ae879c2b347
HugoWinterGhost <hfief1806@gmail.com>
2024-02-05 15:47:29 +0100

    finish completed projet fibonacci

--- Fibonacci/main.go
+++ Fibonacci/main.go
@@ -2,17 +2,40 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
-func readIntFromInput() int {
-	var num int
-	fmt.Print("Veuillez saisir un nombre entier : ")
-	_, err := fmt.Scan(&num)
-	if err != nil {
-		fmt.Println("Erreur lors de la lecture de l'entrée. Veuillez saisir un nombre entier")
-		return 0
+func capitalizeFirstLetter(s string) string {
+	if s == "" {
+		return s
 	}
-	return num
+
+	r := []rune(s)
+	r[0] = unicode.ToUpper(r[0])
+	return string(r)
+}
+
+func readFromInput() (int, error) {
+	var input string
+	fmt.Print("Veuillez saisir un nombre entier : ")
+	_, err := fmt.Scan(&input)
+
+	if err != nil {
+		return 0, fmt.Errorf(capitalizeFirstLetter("erreur lors de la lecture de l'entrée"))
+	}
+
+	if strings.Contains(input, ".") || strings.Contains(input, ",") {
+		return 0, fmt.Errorf(capitalizeFirstLetter("votre nombre contient une virgule, veuillez saisir un nombre entier"))
+	}
+
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		return 0, fmt.Errorf(capitalizeFirstLetter("veuillez saisir un nombre entier valide"))
+	}
+
+	return num, nil
 }
 
 func printFibonacciSeries(n int) {
@@ -24,13 +47,19 @@ func printFibonacciSeries(n int) {
 		if i > 0 {
 			fmt.Print(", ")
 		}
+
 		fmt.Print(term1)
 		term1, term2 = term2, term1+term2
 	}
 }
 
 func main() {
-	definedNumber := readIntFromInput()
+	definedNumber, err := readFromInput()
+
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
 
 	if definedNumber <= 0 {
 		fmt.Print("Nombre incorrect, veuillez saisir un nombre entier strictement positif")
